2578e81a23bdb20f99d0bad06296abce6d580309
jmaralo <martinezalonsojn@gmail.com>
2025-03-11 11:00:51 +0100

    Lets not talk about this :)

--- Core/Src/HVSCU/Communication/Ethernet.cpp
+++ Core/Src/HVSCU/Communication/Ethernet.cpp
@@ -2,8 +2,23 @@
 
 namespace HVSCU::Communication {
 
-Ethernet::Ethernet()
+Ethernet::Ethernet(const float *total_supercaps_voltage,
+                   std::array<std::array<const float *, 48>, 3> cells_voltage,
+                   std::array<const float *, 3> module_voltage,
+                   std::array<const float *, 3> max_cell_voltage,
+                   std::array<const float *, 3> min_cell_voltage,
+                   std::array<const float *, 3> avg_cell_voltage,
+                   std::array<const float *, 3> max_temp,
+                   std::array<const float *, 3> min_temp)
     : control_station_tcp(local_ip, tcp_server_port),
-      control_station_udp(local_ip, udp_port, control_station_ip, udp_port) {}
+      control_station_udp(local_ip, udp_port, control_station_ip, udp_port),
+      total_supercaps_voltage(total_supercaps_voltage),
+      cells_voltage(cells_voltage),
+      module_voltage(module_voltage),
+      max_cell_voltage(max_cell_voltage),
+      min_cell_voltage(min_cell_voltage),
+      avg_cell_voltage(avg_cell_voltage),
+      max_temp(max_temp),
+      min_temp(min_temp) {}
 
 };  // namespace HVSCU::Communication
