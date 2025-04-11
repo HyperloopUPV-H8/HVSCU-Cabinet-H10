3ecc818ba152f17fd752547927d276d8a8085e89
jmaralo <martinezalonsojn@gmail.com>
2025-04-11 23:36:03 +0200

    [WIP] Test Duty Cycle

--- Core/Inc/HVSCU/Communication/CAN.hpp
+++ Core/Inc/HVSCU/Communication/CAN.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
+#include "BCU_data/BCU_data.hpp"
 #include "CMS-LIB.hpp"
 #include "ST-LIB.hpp"
-#include "BCU_data/BCU_data.hpp"
 namespace HVSCU::Communication {
 
 class CAN {
-
     inline constexpr static uint8_t DLC_to_length(
         const ::FDCAN::DLC dlc) noexcept;
 
@@ -14,7 +13,7 @@ class CAN {
         const uint8_t length) noexcept;
 
    public:
-   uint8_t can_id;
+    uint8_t can_id;
 
     CMS::Communications::ModuleCAN module_can;
 
@@ -27,27 +26,22 @@ class CAN {
     void set_data_rate(CMS::Types::TxCycle_t tx_cycle);
 
     void update();
-    void handle_bcu_data(FDCAN::Packet& packet); 
-    FDCAN::Packet test_pwm{.identifier = 0x701,.data_length = FDCAN::DLC::BYTES_3};
-    FDCAN::Packet emulate_movement{.identifier = 0x702,.data_length = FDCAN::DLC::BYTES_6};
-    FDCAN::Packet current_control{
-        .identifier = 0x703,
-        .data_length = FDCAN::DLC::BYTES_4};
-    FDCAN::Packet velocity_control{
-        .identifier = 0x704,
-        .data_length = FDCAN::DLC::BYTES_4};
-    FDCAN::Packet set_pwm_params{
-        .identifier = 0x705,
-        .data_length = FDCAN::DLC::BYTES_8};
-    FDCAN::Packet set_fixed_dc_link_vtg{
-        .identifier = 0x706,
-        .data_length = FDCAN::DLC::BYTES_4};
-    FDCAN::Packet unset_fixed_dc_link_vtg{
-        .identifier = 0x707,
-        .data_length = FDCAN::DLC::BYTES_0};
-    FDCAN::Packet stop{
-        .identifier = 0x708,
-        .data_length = FDCAN::DLC::BYTES_0};
+    void handle_bcu_data(FDCAN::Packet& packet);
+    FDCAN::Packet test_pwm{.identifier = 0x721,
+                           .data_length = FDCAN::DLC::BYTES_3};
+    FDCAN::Packet emulate_movement{.identifier = 0x722,
+                                   .data_length = FDCAN::DLC::BYTES_6};
+    FDCAN::Packet current_control{.identifier = 0x723,
+                                  .data_length = FDCAN::DLC::BYTES_4};
+    FDCAN::Packet velocity_control{.identifier = 0x724,
+                                   .data_length = FDCAN::DLC::BYTES_4};
+    FDCAN::Packet set_pwm_params{.identifier = 0x725,
+                                 .data_length = FDCAN::DLC::BYTES_8};
+    FDCAN::Packet set_fixed_dc_link_vtg{.identifier = 0x726,
+                                        .data_length = FDCAN::DLC::BYTES_4};
+    FDCAN::Packet unset_fixed_dc_link_vtg{.identifier = 0x727,
+                                          .data_length = FDCAN::DLC::BYTES_0};
+    FDCAN::Packet stop{.identifier = 0x728, .data_length = FDCAN::DLC::BYTES_0};
 };
 
 };  // namespace HVSCU::Communication
--- Core/Src/BCU_data/BCU_data.cpp
+++ Core/Src/BCU_data/BCU_data.cpp
@@ -1,4 +1,4 @@
-#include "BCU_Data/BCU_data.hpp"
+#include "BCU_data/BCU_data.hpp"
 float BCU_data::charge_voltage;
 uint8_t BCU_data::duty_u;
 uint8_t BCU_data::duty_v;
@@ -11,15 +11,15 @@ float BCU_data::ref_switch_freq;
 float BCU_data::ref_dead_time;
 float BCU_data::fixed_dc_link_vtg;
 
-    // DATA
+// DATA
 uint8_t BCU_data::master_general_state;
 uint8_t BCU_data::master_nested_state;
 uint8_t BCU_data::slave_general_state;
 uint8_t BCU_data::slave_nested_state;
-std::array<float,25> BCU_data::control_params;
-std::array<float,16> BCU_data::inverters_data;
-std::array<float,12> BCU_data::encoders_data;
-std::array<uint8_t,8> BCU_data::encoders_control;
+std::array<float, 25> BCU_data::control_params;
+std::array<float, 16> BCU_data::inverters_data;
+std::array<float, 12> BCU_data::encoders_data;
+std::array<uint8_t, 8> BCU_data::encoders_control;
 uint8_t BCU_data::direction_speetec_1;
 uint8_t BCU_data::direction_speetec_2;
 uint8_t BCU_data::direction_speetec_3;
