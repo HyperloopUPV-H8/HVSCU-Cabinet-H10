e1234dba51c3d3e3b03c12c2bf0a639c02a4736b
Cantonplas <dcancat@etsinf.upv.es>
2025-12-02 18:05:49 +0100

    Working

--- Core/Inc/HVSCU/Actuators/Contactors.hpp
+++ Core/Inc/HVSCU/Actuators/Contactors.hpp
@@ -19,7 +19,7 @@ class Contactors {
     };
 
    private:
-    constexpr static float SAFE_VOLTAGE_DIFF{20.0f};
+    constexpr static float SAFE_VOLTAGE_DIFF{0.95f};
     constexpr static uint32_t CHECK_PERIOD_MS{100};
     inline static volatile bool flag_polla {false};
 
